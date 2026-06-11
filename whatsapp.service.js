40cea76a03e775e57932b2fb563db09e0b7002bd
Dhiraj2007 <dhirajtambe2007@gmail.com>
2026-06-11 17:19:49 +0530

    fix: remove single-process to stop instant LOGOUT bug

--- whatsapp.service.js
+++ whatsapp.service.js
@@ -63,11 +63,12 @@ class WhatsappService {
             "--no-sandbox",
             "--disable-setuid-sandbox",
             "--disable-dev-shm-usage",
+            "--disable-accelerated-2d-canvas",
+            "--no-first-run",
             "--no-zygote",
-            "--single-process"
+            "--disable-gpu"
           ]
-        },
-        userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        }
       });
 
       this.client.on("qr", async (qr) => {
